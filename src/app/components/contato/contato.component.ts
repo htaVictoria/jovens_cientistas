667d7fd8f3e59f7f3359225011aa592c9ca41827
htaVictoria <agatharocha0120@gmail.com>
2025-11-24 16:43:52 -0300

    pagina contato

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 
 @Component({
   selector: 'app-contato',
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent {
+  toppings = new FormControl('');
+  toppingList: string[] = ['Ciencias da natureza', 'Matematica', 'Robotica'];
 
 }
